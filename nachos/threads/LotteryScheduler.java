792bce1e1d9b1f15917d949d20b26507472870eb
Tlaloc <tbarajas3@ucmerced.edu>
2018-12-02 13:14:39 -0800

    task 4 update
    task 4 update, not complete...

--- nachos/threads/LotteryScheduler.java
+++ nachos/threads/LotteryScheduler.java
@@ -1,6 +1,7 @@
 package nachos.threads;
 
 import nachos.machine.*;
+import nachos.threads.PriorityScheduler.ThreadState;
 
 import java.util.TreeSet;
 import java.util.HashSet;
@@ -42,7 +43,54 @@ public class LotteryScheduler extends PriorityScheduler {
      * @return	a new lottery thread queue.
      */
     public ThreadQueue newThreadQueue(boolean transferPriority) {
-	// implement me
-	return null;
+		return new LotteryQueue(transferPriority); //lotteryQUEUE
     }
+    
+    protected ThreadState getThreadState(KThread thread) {
+	if (thread.schedulingState == null)
+	    thread.schedulingState = new ThreadState(thread);
+
+	return (ThreadState) thread.schedulingState;
+    }
+    
+	public void setPriority(KThread thread, int priority) { // change this
+		Lib.assertTrue(Machine.interrupt().disabled());
+
+		Lib.assertTrue(priority >= priorityMinimum
+				&& priority <= priorityMaximum);
+
+		getThreadState(thread).setPriority(priority);
+	}
+
+    public boolean increasePriority() {
+	boolean intStatus = Machine.interrupt().disable();
+		       
+	KThread thread = KThread.currentThread();
+
+	int priority = getPriority(thread);
+	if (priority == priorityMaximum)
+	    return false;
+
+	setPriority(thread, priority+1);
+
+	Machine.interrupt().restore(intStatus);
+	return true;
+    }
+    
+    public boolean decreasePriority() {
+	boolean intStatus = Machine.interrupt().disable();
+		       
+	KThread thread = KThread.currentThread();
+
+	int priority = getPriority(thread);
+	if (priority == priorityMinimum)
+	    return false;
+
+	setPriority(thread, priority-1);
+
+	Machine.interrupt().restore(intStatus);
+	return true;
+    }
+    
+    
 }
